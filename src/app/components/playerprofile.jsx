5b31248f29b043bbf4a28cd76b3a4094b595112a
Yannickqb <yanni.ledermann@gmail.com>
2023-11-07 11:31:26 +0100

    fixede some problems with offencif roster

--- src/app/components/playerprofile.jsx
+++ src/app/components/playerprofile.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 
 export default function PlayerProfile({ position, name, image, number, nationalityIcon, buttonText, onButtonClick }) {
+  // Styles to ensure the image height is 140px, border radius, and left margin
+  const imageStyle = { height: '140px', borderRadius: '0.5rem', marginLeft: '1rem' };
+  // Style for 75% opacity
+  const cardStyle = { backgroundColor: 'rgba(0, 0, 0, 0.75)' }; // Adjust the rgba values as needed to change the color and opacity
+
   return (
-    <div className="card card-side bg-base-100 shadow-xl">
-      <figure>
-        <img src={image} alt={name} />
+    <div className="card lg:card-side shadow-xl w-3/4 mx-auto" style={cardStyle}>
+      <figure className="ml-4">
+        <img src={image} alt={`Profile of ${name}`} style={imageStyle} />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{position}</h2>
-        <p>{name}</p>
-        {number && <p>#{number}</p>}
-        {nationalityIcon && <img src={nationalityIcon} alt="Nationality" />}
+        {/* Position and Number on one line, Name on the next line */}
+        <div className="mb-2">
+          <span className="font-bold">{position} #{number}</span>
+        </div>
+        <div className="mb-4">
+          <h2 className="card-title">{name}</h2>
+        </div>
+        <div>
+          <img src={nationalityIcon} alt="Nationality" style={{ height: '24px' }} />
+        </div>
         <div className="card-actions justify-end">
           <button className="btn" onClick={onButtonClick}>{buttonText}</button>
         </div>
--- src/pages/offenceroster.js
+++ src/pages/offenceroster.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PlayerProfile from '@/app/components/playerprofile';
+import Navbar from '@/app/components/nav';
+import Footer from '@/app/components/footer';
 
 const players = [
   {
@@ -14,18 +16,37 @@ const players = [
   {
     id: "p2",
     position: "RB",
-    name: "Arius Grey",
-    image: "/offence2.png",
+    name: "Marc Hermann",
+    image: "/offence.png",
     number: "22",
     nationalityIcon: "/path-to-nationality-icon2.jpg",
     buttonText: "View Profile"
   },
-  // ... other players
+  
+  {
+    id: "p3",
+    position: "RB",
+    name: "Alessio sacullo",
+    image: "/offence.png",
+    number: "22",
+    nationalityIcon: "/path-to-nationality-icon2.jpg",
+    buttonText: "View Profile"
+  },
+
+  {
+    id: "p4",
+    position: "RB",
+    name: "Arius Grey",
+    image: "/offence.png",
+    number: "22",
+    nationalityIcon: "/path-to-nationality-icon2.jpg",
+    buttonText: "View Profile"
+  },
   {
     id: "p20",
     position: "WR",
     name: "Xavier Enderson",
-    image: "/offence20.png",
+    image: "/offence.png",
     number: "88",
     nationalityIcon: "/path-to-nationality-icon20.jpg",
     buttonText: "View Profile"
@@ -39,19 +60,34 @@ export default function OffenseRoster() {
   };
 
   return (
-    <div>
-      {players.map((player, index) => (
-        <PlayerProfile
-          key={player.id}
-          position={player.position}
-          name={player.name}
-          image={player.image}
-          number={player.number}
-          nationalityIcon={player.nationalityIcon}
-          buttonText={player.buttonText}
-          onButtonClick={() => handleButtonClick(player.id)}
-        />
-      ))}
+    <div className="relative min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url("/Background.png")' }}>
+      {/* Sticky Navbar */}
+      <div className="fixed top-0 left-0 w-full z-50">
+        <Navbar />
+      </div>
+      
+      {/* Title */}
+      <div className="text-center pt-20"> {/* Adjust padding-top as necessary */}
+        <h1 className="text-6xl font-bold text-white">SHARKS</h1>
+      </div>
+
+      {/* Spacing for the first component */}
+      <div className="pt-16"> {/* This adds padding-top to make space for the sticky navbar */}
+        {players.map((player, index) => (
+          <div key={player.id} className="my-4"> {/* This adds margin to the y-axis (top and bottom) for each PlayerProfile component */}
+            <PlayerProfile
+              position={player.position}
+              name={player.name}
+              image={player.image}
+              number={player.number}
+              nationalityIcon={player.nationalityIcon}
+              buttonText={player.buttonText}
+              onButtonClick={() => handleButtonClick(player.id)}
+            />
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }
